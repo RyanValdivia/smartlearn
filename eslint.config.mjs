87c011253beae41fdafc67e7feb95a26dfd2b3b9
= <admamaniq@unsa.edu.pe>
2025-02-27 20:42:29 -0500

    feat:better lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,36 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const skipLint = false;
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "plugin:@typescript-eslint/recommended"),
+  {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+    },
+    plugins: ["@typescript-eslint", "custom-rules"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": skipLint
+        ? "off"
+        : [
+            "error",
+            {
+              prefer: "type-imports",
+              fixStyle: "inline-type-imports",
+            },
+          ],
+      "@typescript-eslint/no-unused-vars": skipLint
+        ? "off"
+        : ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": skipLint ? "off" : "error",
+      "custom-rules/no-object-concat": "off",
+      "custom-rules/no-prisma-enums": "error",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
