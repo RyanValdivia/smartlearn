c8cff1ea9a885d52ac92e40a86145df33b289b4f
RyanValdivia <rvaldiviase@unsa.edu.pe>
2025-03-09 20:52:04 -0500

    fix: dni is now required

--- src/core/modules/auth/actions.ts
+++ src/core/modules/auth/actions.ts
@@ -5,7 +5,5 @@ import { getInjection } from "@/core/di/container";
 export async function signIn() {
     const userController = getInjection("UsersController");
 
-    // return await userController.createUser({
-    //     data
-    // });
+    return await userController.createUser({});
 }
